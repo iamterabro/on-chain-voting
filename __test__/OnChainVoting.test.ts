e2f64ae64d509786300ad14ae88ff9c0cdfba7a1
iamterabro <iamterabro2000@gmail.com>
2025-02-03 09:26:19 +0000

    CI configs

--- __test__/OnChainVoting.test.ts
+++ __test__/OnChainVoting.test.ts
@@ -2,6 +2,7 @@ import { beforeAll, describe, expect, test } from '@jest/globals';
 import { algorandFixture } from '@algorandfoundation/algokit-utils/testing';
 import { AlgorandClient, Config } from '@algorandfoundation/algokit-utils';
 import { AlgoAmount } from '@algorandfoundation/algokit-utils/types/amount';
+import { Account, generateAccount, makeBasicAccountTransactionSigner } from 'algosdk';
 import { OnChainVotingClient, OnChainVotingFactory } from '../contracts/clients/OnChainVotingClient';
 
 const fixture = algorandFixture();
@@ -31,50 +32,68 @@ describe('OnChainVoting', () => {
 
   describe('happy path flow', () => {
     let appClient: OnChainVotingClient;
+    let eligibleVotingAccount: Account;
+    let nonEligibleVotingAccount: Account;
 
     test('creates the application', async () => {
       appClient = await createAndFundApplication(clientFactory, algorandClient);
     });
 
-    test('adds voting options', async () => {
-      await appClient.send.addOption({ args: { option: BigInt(1), description: 'Option 1' } });
-      await appClient.send.addOption({ args: { option: BigInt(2), description: 'Option 2' } });
+    test('creates voting accounts', async () => {
+      eligibleVotingAccount = await createAccount(algorandClient);
+      nonEligibleVotingAccount = await createAccount(algorandClient);
     });
 
-    test('adds a voter', async () => {
-      await appClient.send.addVoter({
-        args: { votingPower: BigInt(1), voterAddress: fixture.context.testAccount.addr },
-      });
+    test('adds voting options', async () => {
+      await addVotingOption(appClient, { optionId: 1, description: 'Option 1' });
+      await addVotingOption(appClient, { optionId: 2, description: 'Option 2' });
+    });
+
+    test('adds voters', async () => {
+      await addVoter(appClient, { address: fixture.context.testAccount.addr, votingPower: 1 });
+      await addVoter(appClient, { address: eligibleVotingAccount.addr, votingPower: 2 });
     });
 
     test('starts the voting', async () => {
       const start = BigInt(Math.floor(Date.now() / 1000) - 10000);
       const end = BigInt(Math.floor(Date.now() / 1000) + 10000);
-      await appClient.send.updateTimes({ args: { start, end } });
-      await sleep(1000);
+      await updateVotingTimes(appClient, start, end);
     });
 
-    test('votes', async () => {
-      await appClient.send.vote({
-        args: { choice: BigInt(1), votingPower: BigInt(1) },
-        populateAppCallResources: true,
-      });
+    test('eligible users votes', async () => {
+      await vote(appClient.appId, { choice: 1, votingPower: 1 }, fixture.context.testAccount);
+      await vote(appClient.appId, { choice: 2, votingPower: 2 }, eligibleVotingAccount);
     });
 
     test('cannot vote again', async () => {
       await expect(
-        appClient.send.vote({
-          args: { choice: BigInt(1), votingPower: BigInt(1) },
-          populateAppCallResources: true,
-        })
+        vote(
+          appClient.appId,
+          {
+            choice: 2,
+            votingPower: 2,
+          },
+          eligibleVotingAccount
+        )
       ).rejects.toThrow(/logic eval error/);
+    });
+
+    test('non-eligible user cannot vote', async () => {
+      await expect(vote(appClient.appId, { choice: 1, votingPower: 1 }, nonEligibleVotingAccount)).rejects.toThrow(
+        /logic eval error/
+      );
     });
   });
 });
 
-function sleep(ms: number) {
-  return new Promise<void>((resolve) => {
-    setTimeout(resolve, ms);
+async function vote(appId: bigint, userVote: { choice: number; votingPower: number }, account: Account): Promise<void> {
+  const appClient = clientFactory.getAppClientById({ appId });
+  const signer = makeBasicAccountTransactionSigner(account);
+  await appClient.send.vote({
+    args: { choice: BigInt(userVote.choice), votingPower: BigInt(userVote.votingPower) },
+    populateAppCallResources: true,
+    sender: account.addr,
+    signer,
   });
 }
 
@@ -94,4 +113,31 @@ async function createAndFundApplication(
   );
 
   return appClient;
+}
+
+async function addVoter(appClient: OnChainVotingClient, voter: { address: string; votingPower: number }) {
+  await appClient.send.addVoter({
+    args: { votingPower: BigInt(voter.votingPower), voterAddress: voter.address },
+  });
+}
+
+async function updateVotingTimes(appClient: OnChainVotingClient, start: bigint, end: bigint) {
+  await appClient.send.updateTimes({ args: { start, end } });
+  await sleep(1000);
+}
+
+async function addVotingOption(appClient: OnChainVotingClient, option: { optionId: number; description: string }) {
+  await appClient.send.addOption({ args: { option: BigInt(option.optionId), description: option.description } });
+}
+
+function sleep(ms: number) {
+  return new Promise<void>((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
+
+async function createAccount(algorand: AlgorandClient): Promise<Account> {
+  const acct = generateAccount();
+  await algorand.account.ensureFunded(acct.addr, await algorand.account.localNetDispenser(), AlgoAmount.Algos(5));
+  return acct;
 }
